b4a393407397623482f0a2e0e445e7b61f15f915
Andrey Camargo <andreycamargo9@gmail.com>
2024-04-01 21:51:42 -0300

    Tabela Atualizada

--- table/src/components/PriceList.js
+++ table/src/components/PriceList.js
@@ -4,25 +4,44 @@ import {useState} from 'react';
 
 
 function PriceList() {
-    let id_produto_1 = 1;
-    let id_produto_2 = 2;
-    let id_produto_3 = 3;
-    var nome_produto1 = "Produto 1";
-    let nome_produto2 = "Produto 2";
-    let nome_produto3 = "Produto 3";
-    let id_categoria_1 = "Categoria 1";
-    let id_categoria_2 = "Categoria 2";
-    let id_categoria_3 = "Categoria 3";
-    let qtde_1 = 5 ;
-    let qtde_2 = 4 ;
-    let qtde_3 = 3 ;
-    let preco_unitario1 = "R$ 10.00" ;
-    let preco_unitario2 = "R$ 35.00" ;
-    let preco_unitario3 = "R$ 41.00" ;
-    let total1 = qtde_1 * preco_unitario1 ;  
-    let total2 = qtde_2 * preco_unitario2 ; 
-    let total3 = qtde_3 * preco_unitario3 ;
-    let total = total1 + total2 + total3
+    let produto1 = {
+        id: 1,
+        nome_produto1: "Produto 1",
+        categoria: "Categoria 1",
+        qtde: 5,
+        preco_unitario: 10.00,
+        total: 50,
+    }
+
+    let produto2 = {
+        id: 2,
+        nome_produto: "Produto 2",
+        categoria: "Categoria 2",
+        qtde: 4,
+        preco_unitario: 35.00,
+        total: 140,
+    }
+
+    let produto3 = {
+        id: 3,
+        nome_produto: "Produto 3",
+        categoria: "Categoria 3",
+        qtde: 3,
+        preco_unitario: 41.00,
+        total3: 164,
+    }
+
+    let lista_produto = {
+        produto1,
+        produto2,
+        produto3
+    }
+
+    lista_produto.map(
+        function (produto){
+            return 
+        }
+    )
 
     return (
         <div>
@@ -40,35 +59,30 @@ function PriceList() {
                         <td className='direita'>Total</td>
                     </tr>
                     <tr className='claro'>
-                        <td>{id_produto_1}</td>
-                        <td>{nome_produto1}</td>
-                        <td>{id_categoria_1}</td>
-                        <td className='direita'>{qtde_1}</td>
-                        <td className='direita'>{preco_unitario1}</td>
-                        <td className='direita'>{total1}</td>
+                        <td>{produto1.id}</td>
+                        <td>{produto1.nome_produto1}</td>
+                        <td>{produto1.categoria}</td>
+                        <td className='direita'>{produto1.qtde}</td>
+                        <td className='direita'>{produto1.preco_unitario}</td>
+                        <td className='direita'>{produto1.total}</td>
                     </tr>
                     <tr className='escuro'>
-                        <td>{id_produto_2}</td>
-                        <td>{nome_produto2}</td>
-                        <td>{id_categoria_2}</td>
-                        <td className='direita'>{qtde_2}</td>
-                        <td className='direita'>{preco_unitario2}</td>
-                        <td className='direita'>{total2}</td>
+                        <td>{produto2.id}</td>
+                        <td>{produto2.nome_produto}</td>
+                        <td>{produto2.categoria}</td>
+                        <td className='direita'>{produto2.qtde}</td>
+                        <td className='direita'>{produto2.preco_unitario}</td>
+                        <td className='direita'>{produto2.total}</td>
                     </tr>
                     <tr className='claro'>
-                        <td>{id_produto_3}</td>
-                        <td>{nome_produto3}</td>
-                        <td>{id_categoria_3}</td>
-                        <td className='direita'>{qtde_3}</td>
-                        <td className='direita'>{preco_unitario3}</td>
-                        <td className='direita'>{total3}</td>
+                        <td>{produto3.id}</td>
+                        <td>{produto3.nome_produto}</td>
+                        <td>{produto3.categoria}</td>
+                        <td className='direita'>{produto3.qtde}</td>
+                        <td className='direita'>R$ {produto3.preco_unitario}</td>
+                        <td className='direita'>R$ {produto3.total}</td>
                     </tr>
-                    <tr className='escuro'>
-                        <td colspan="3"></td>
-                        <td className='direita'>12</td>
-                        <td className='direita'>R$ 86,00</td>
-                        <td className='direita'>{total}</td>
-                    </tr>
+                   
                 </tbody>
             </table>
             <br />
