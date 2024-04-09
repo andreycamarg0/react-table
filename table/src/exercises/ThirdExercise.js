bf3047fef2291aa554665c9b552e05e6e0975a04
Andrey Camargo <andreycamargo9@gmail.com>
2024-04-08 21:58:39 -0300

    exercicio 4

---
+++ table/src/exercises/ThirdExercise.js
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+
+
+function ThirdExercise() {
+
+    let [Jogos, setJogos] = useState();
+
+    let [nome, setNome] = useState();
+
+    let vitoria = Jogos / 2 * 3
+    let empate = Jogos / 2 * 1
+    let total = vitoria + empate
+
+
+    function alterarNome(elemento) {
+        setNome(elemento.target.value)
+    }
+    function alterarJogos(elemento) {
+        setJogos(elemento.target.value) 
+    }
+        
+        return (
+            <div class="container">
+                <div>Nome do time:<input onChange={alterarNome} type= "text" placeholder = "Nome do Time"></input></div>
+                <p>Quantidades de jogos:<input onChange={alterarJogos} type = "number" placeholder='Quantidade de Jogos' disabled = {!nome}></input></p>
+                <p>Pontos de vitoria: {vitoria}</p>
+                <p>Pontos de empate: {empate}</p>
+                <p>Total de pontos: {total}</p>
+            </div>
+        );
+
+
+
+    
+     
+    
+  
+}
+    export default ThirdExercise;
--- table/src/pages/PrincipalPage.js
+++ table/src/pages/PrincipalPage.js
@@ -1,11 +1,16 @@
 import FirstExercise from "../exercises/FirstExercise";
 import SecondExercise from "../exercises/SecondExercise";
+import ThirdExercise from "../exercises/ThirdExercise";
+import FourthExercise from "../exercises/FourthExercise"
+
 
 function PrincipalPage() {
     return (
       <div>
         <FirstExercise></FirstExercise> 
         <SecondExercise></SecondExercise>
+        <ThirdExercise></ThirdExercise>
+        <FourthExercise></FourthExercise>
       </div>
     );
   }
